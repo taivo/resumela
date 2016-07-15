da253bbfffe2d481f16cf7ff56843e59fb5ebcd2
taivo <tai.a.vo@gmail.com>
2016-07-14 19:30:52 -0700

    added duration and better time formatting

--- app/shared/item/itemDirective.js
+++ app/shared/item/itemDirective.js
@@ -24,6 +24,29 @@ function(){
         return typeToTag[lower] || lower;
       }
 
+      if($scope.item.startDate && $scope.item.endDate){
+          var startM = moment($scope.item.startDate);
+          var endM   = moment($scope.item.endDate);
+
+          if(!endM.isValid()){
+              endM = moment();
+          }
+
+          $scope.item.startDateStr = startM.format('MMM YYYY');
+          $scope.item.endDateStr   = endM.format('MMM YYYY');
+
+
+
+          var years = Math.round(10 * endM.diff(startM, 'years', true)) /10;
+          if(years <= 1){
+              $scope.item.durationStr = years + ' year';
+          } else{
+              $scope.item.durationStr = [years, 'years'].join(' ');
+          }
+          //var months = Math.round(10 * moment($scope.item.endDate).diff($scope.item.startDate, 'months', true)) /10;
+          //$scope.item.durationStr = [Math.floor(months/12), 'year', months % 12, 'months'].join(' ');
+      }
+
       $scope.getTemplateUrl = function(){
         var itemTag = itemTypeToTag($scope.itemType);
 
